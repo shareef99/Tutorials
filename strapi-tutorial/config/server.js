3ca91674a563d919d1c28d7c0e75fad187563e9f
shareef <nadeemshareef934@gmail.com>
2021-08-14 14:30:26 +0530

    add strapi tutorial

---
+++ strapi-tutorial/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '44169eacc8c621374fe777ba85cee7c9'),
+    },
+  },
+});
---
+++ strapi-tutorial/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '83c91554-163b-48cc-b1a9-dda753d3fbbf'
+};
