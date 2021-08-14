004756cf15eb37695039a6389bf15e8881aef86b
shareef <nadeemshareef934@gmail.com>
2021-08-14 17:43:31 +0530

    Add strapi backend tutorila

---
+++ strapi-backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '677984c30222498ef979b6513abc3419'),
+    },
+  },
+});
---
+++ strapi-backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '18b9f636-14ef-4a29-8fe2-913b1d546224'
+};
